484856be153229a0b507c734605bf320fd49111a
Melqui Vunge <melquilegend@gmail.com>
2021-08-11 15:07:00 +0100

    CLI app

--- project _friend_ist/app.js
+++ project _friend_ist/app.js
@@ -1,26 +1,23 @@
 const app = Vue.createApp({
-	data(){
-		return{
-			friends:[
-				{
-					id:'1', 
-					name:'Andreceu Samba', 
-					phone:'+26482554125', 
-					email:'andreceu@gmail.com'
-				},
-				{
-					id:'2', 
-					name:'Karoline Kamana', 
-					phone:'+26482554125', 
-					email:'Karoline@gmail.com'
-				},
-			],
-		};
-	},
+    data() {
+        return {
+            friends: [{
+                id: '1',
+                name: 'Andreceu Samba',
+                phone: '+26482554125',
+                email: 'andreceu@gmail.com'
+            }, {
+                id: '2',
+                name: 'Karoline Kamana',
+                phone: '+26482554125',
+                email: 'Karoline@gmail.com'
+            }, ],
+        };
+    },
 });
 
 app.component('friend-contacto', {
-	template:`
+    template: `
         <li>
           <h2>{{friend.name}}</h2>
           <button @click="toggleDetails">{{detailAreVisible ? 'Hide' : 'Show'}} Details</button>
@@ -29,23 +26,23 @@ app.component('friend-contacto', {
             <li><strong>Email:</strong> {{friend.email}}</li>
           </ul>
         </li>
-	`, 
-	data(){
-		return {
-			detailAreVisible:false,
-			friend: {
-					id:'1', 
-					name:'Andreceu Samba', 
-					phone:'+26482554125', 
-					email:'andreceu@gmail.com'
-				},
-		};
-	},
-		methods: {
-		toggleDetails(){
-			this.detailAreVisible = !this.detailAreVisible;
-		}
-	}
+	`,
+    data() {
+        return {
+            detailAreVisible: false,
+            friend: {
+                id: '1',
+                name: 'Andreceu Samba',
+                phone: '+26482554125',
+                email: 'andreceu@gmail.com'
+            },
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailAreVisible = !this.detailAreVisible;
+        }
+    }
 
 });
 app.mount('#app');
